bddfb338b50299b8a7f36bcce9410fe478e88548
ixtiyoruz <ixtiyoruz312@gmail.com>
2019-09-24 21:26:55 +0900

    copy master )))

---
+++ make.sh
@@ -0,0 +1,22 @@
+cd tracker
+sh makelib.sh
+
+cd ..
+
+cd darknet-master
+make -j8
+
+cd ..
+
+sudo cp ./darknet-master/libdarknet.so ./
+sudo cp ./tracker/libdeepsort.so ./
+
+sudo cp libdarknet.so /usr/lib/
+sudo cp libdeepsort.so /usr/lib/
+
+cd code
+make -j8
+
+cd ..
+
+cp code/yolo_console_dll ./
